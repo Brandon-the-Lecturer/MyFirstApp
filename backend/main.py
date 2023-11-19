5bf689e0a3d3095625e9dce3168e7686c34d8520
brandon <brandon.sai@hs-aalen.de>
2023-11-19 15:49:01 +0100

    feat: initial backend for level 1

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+import json
+from fastapi import FastAPI
+
+PATH = "data/nfl_data.json"
+
+
+app = FastAPI()
+
+
+@app.get("/data")
+async def get_data():
+    with open(file=PATH, mode="r") as raw_file:
+        data = json.load(raw_file)
+    return data
