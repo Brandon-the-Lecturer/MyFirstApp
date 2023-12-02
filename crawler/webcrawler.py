e86e975542e26321461768f0984f737d2e386a46
brandon <brandon.sai@hs-aalen.de>
2023-12-02 15:44:41 +0100

    feat: add html parser

--- crawler/webcrawler.py
+++ crawler/webcrawler.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from requests import Session, RequestException
 
 URL = "https://www.pro-football-reference.com"
@@ -21,10 +22,16 @@ def request_url(session: Session, url: str, retries: int = 3, timeout: int = 10)
     return
 
 
+def parse_html_to_soup(html: str) -> BeautifulSoup:
+    return BeautifulSoup(html, "html.parser")
+
+
 def main():
     http_session = Session()
     html = request_url(session=http_session, url=URL)
-    print(html)
+    soup = parse_html_to_soup(html)
+
+    print(soup)
 
 
 if __name__ == "__main__":
